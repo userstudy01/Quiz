0a9202661e7187b088fcdebb85772c45483072a5
vinitathakkar <vinitathakkar75@gmail.com>
2026-04-11 23:06:51 +0530

    some changes

--- Frontend/src/components/dashboard/QuizView.jsx
+++ Frontend/src/components/dashboard/QuizView.jsx
@@ -3,6 +3,7 @@ import confetti from 'canvas-confetti';
 import { fetchProgress, saveProgress } from '../../features/api';
 import Sidebar from './Sidebar';
 import StatsPanel from './StatsPanel';
+import { API_URL } from '../../features/api';
 
 export default function QuizView({ user, questions, selectedRound, setSelectedRound }) {
   const [userAnswers, setUserAnswers] = useState({});
@@ -213,9 +214,9 @@ export default function QuizView({ user, questions, selectedRound, setSelectedRo
     try {
       const storedUser = JSON.parse(localStorage.getItem('user')) || {};
       const token = user?.token || storedUser?.token || ""; 
-      const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+      // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
-      const response = await fetch(`${API_BASE_URL}/api/evaluations/save`, {
+      const response = await fetch(`${API_URL}/api/evaluations/save`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
         body: JSON.stringify({ moduleName: selectedRound, userAnswers, scores: { ...scores, theory: tTheory, practical: tPractical }, attempts, history })
