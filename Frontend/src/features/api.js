6df0998936ec89ce8f1d38ffbd763a3de02689fd
vinitathakkar <vinitathakkar75@gmail.com>
2026-04-11 23:18:21 +0530

    some changes

--- Frontend/src/features/api.js
+++ Frontend/src/features/api.js
@@ -39,7 +39,7 @@
 import axios from 'axios';
 
 // 🔥 LOCALHOST KO HATAKAR VERCEL WALA LINK DAAL DIYA HAI
-const API_URL = 'https://quiz-kappa-dusky.vercel.app/api/';
+export const API_URL = 'https://quiz-kappa-dusky.vercel.app/api/';
 
 // Request main JWT token bhejne ke liye helper
 const getAuthConfig = (thunkAPI) => {
