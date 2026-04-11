f1dd8ba19ac8066c1048a1dabdcaa5a85207ec4d
vinitathakkar <vinitathakkar75@gmail.com>
2026-04-11 23:23:33 +0530

    some changes

--- Frontend/src/components/dashboard/QuizView.jsx
+++ Frontend/src/components/dashboard/QuizView.jsx
@@ -216,7 +216,7 @@ export default function QuizView({ user, questions, selectedRound, setSelectedRo
       const token = user?.token || storedUser?.token || ""; 
       // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
-      const response = await fetch(`${API_URL}api/evaluations/save`, {
+      const response = await fetch(`${API_URL}evaluations/save`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
         body: JSON.stringify({ moduleName: selectedRound, userAnswers, scores: { ...scores, theory: tTheory, practical: tPractical }, attempts, history })
